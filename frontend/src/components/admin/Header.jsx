f954e3fdf48891b1c8e37f20cebf8ba3f79620c8
Leny15 <stevynguyen@gmail.com>
2026-07-03 13:29:27 +0200

    feat: rend le bouton logout opérationel

--- frontend/src/components/admin/Header.jsx
+++ frontend/src/components/admin/Header.jsx
@@ -1,7 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from '@/store/authSlice';
 import LogoHeader from '@/assets/argentBankLogo.png'
 
+
 const Header = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    const isLogout = () => {
+        dispatch(logout())
+        navigate('/sign-in')
+    }
+
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
@@ -9,11 +22,18 @@ const Header = () => {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link className="main-nav-item" to="/sign-in">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </Link>
-                <button>Logout</button>
+                {isAuthenticated ? (
+                    <button className="main-nav-item" onClick={isLogout}>
+                        <i className="fa fa-sign-out"></i>
+                        Sign Out
+                    </button>
+                ) : (
+                    <Link className="main-nav-item" to="/sign-in">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                    </Link>
+                )}
+                <button onClick={isLogout}>Logout</button>
             </div>
         </nav>
     );
