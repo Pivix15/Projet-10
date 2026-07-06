c60895a724ffddeaf6054e9c17b075bbca87ce1f
Leny15 <stevynguyen@gmail.com>
2026-07-06 20:04:33 +0200

    refactor: fusion du header et correction des styles
    - Suppression du header dédié à l'administration
    - Utilisation d'un seul header pour les espaces public et admin
    - Correction des styles CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,43 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from '@/store/authSlice';
 import LogoHeader from '@/assets/argentBankLogo.png'
 
+
 const Header = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isLogged = useSelector((state) => state.auth.isLogged);
+    const firstName = useSelector((state) => state.auth.firstName)
+
+    const isLogout = () => {
+        dispatch(logout())
+        localStorage.removeItem('token')
+        navigate('/sign-in')
+    }
+
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src={LogoHeader} alt="Argent Bank Logo"/>
+                <img className="main-nav-logo-image" src={LogoHeader} alt="Argent Bank Logo" />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <NavLink className="main-nav-item" to="/sign-in">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </NavLink>
+                {isLogged ? (
+                    <div className="header-user">
+                        <p><i className="fa fa-user-circle"></i>{firstName}</p>
+                        <button className="main-nav-item" onClick={isLogout}>
+                            <i className="fa fa-sign-out"></i>
+                            <span className="main-nav-text">Sign Out</span>
+                        </button>
+                    </div>
+                ) : (
+                    <Link className="main-nav-item" to="/sign-in">
+                        <i className="fa fa-user-circle"></i>
+                        <span className="main-nav-text">Sign In</span>
+                    </Link>
+                )}
             </div>
         </nav>
     );
--- frontend/src/pages/Admin/ALayout.jsx
+++ frontend/src/pages/Admin/ALayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "@/components/admin/Header"
+import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
 const ALayout = () => {
