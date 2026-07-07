d17682c1a998d64243f4a2bd37244c613159fa11
Leny15 <stevynguyen@gmail.com>
2026-07-07 16:05:48 +0200

    feat: ajout de l'édition du firstname utilisateur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
 import '@/style/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginSuccess, setUser } from './store/authSlice'
 import PublicRouter from '@/pages/Public/PublicRouter'
 import AdminRouter from '@/pages/Admin/AdminRouter'
 import AuthGuard from '@/_helpers/AuthGuard'
-import { useDispatch } from 'react-redux'
-import { loginSuccess, setUser } from './store/authSlice'
-
 
 function App() {
-
   const dispatch = useDispatch()
-  const token = localStorage.getItem('token')
-  if (token) {
-    dispatch(loginSuccess(token))
+  const init = useRef(null) // ne déclenche pas de re-render
 
-    fetch('http://localhost:3001/api/v1/user/profile', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    })
+  if (init.current == null) {
+    init.current = true
+    
+    const token = localStorage.getItem('token')
+    if (token) {
+      dispatch(loginSuccess(token))
+      fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
       .then(res => res.json())
       .then(profile => {
         dispatch(setUser({
@@ -28,6 +31,8 @@ function App() {
           lastName: profile.body.lastName
         }))
       })
+      .catch(err => console.log(err))
+    }
   }
 
   return (
--- frontend/src/pages/Admin/User/Profile.jsx
+++ frontend/src/pages/Admin/User/Profile.jsx
@@ -1,14 +1,80 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '@/store/authSlice'
+import { useState } from 'react'
 
 const Profile = () => {
     const firstName = useSelector((state) => state.auth.firstName)
     const lastName = useSelector((state) => state.auth.lastName)
 
+    const dispatch = useDispatch()
+    const token = useSelector((state) => state.auth.token)
+
+    const [editing, setEditing] = useState(false)
+    const [newFirstName, setNewFirstName] = useState('')
+
+    const handleEdit = () => {
+        setEditing(true)
+        setNewFirstName(firstName)
+    }
+
+    const handleSave = () => {
+        fetch('http://localhost:3001/api/v1/user/profile', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                firstName: newFirstName,
+                lastName: lastName
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('lastName depuis Redux:', lastName)
+                dispatch(setUser({
+                    firstName: newFirstName,
+                    lastName: data.body.lastName
+                }))
+                setEditing(false)
+            })
+    }
+
     return (
         <main className="main bg-dark">
             <div className="header">
-                <h1>Welcome back<br />{firstName} {lastName}!</h1>
-                <button className="edit-button">Edit Name</button>
+                {editing ? (
+                    <>
+                        <h1>Welcome back</h1>
+                        <div className='edit-form'>
+                            <div className="edit-form-row">
+                                <label>First name:</label>
+                                <input
+                                    type="text"
+                                    value={newFirstName}
+                                    onChange={(e) => setNewFirstName(e.target.value)}
+                                />
+                            </div>
+                            <div className="edit-form-row">
+                                <label>Last name:</label>
+                                <input
+                                    type="text"
+                                    value={lastName}
+                                    readOnly
+                                />
+                            </div>
+                            <div className='main-button'>
+                                <button className="edit-button" onClick={handleSave}>Save</button>
+                                <button className="edit-button" onClick={() => setEditing(false)}>Cancel</button>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <h1>Welcome back<br />{firstName} {lastName}!</h1>
+                        <button className="edit-button" onClick={handleEdit}>Edit Name</button>
+                    </>
+                )}
             </div>
             <h2 className="sr-only">Accounts</h2>
             <section className="account">
