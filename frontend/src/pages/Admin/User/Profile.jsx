fc2f6bb6104e4babf25203bbe908cce7e1c94213
Leny15 <stevynguyen@gmail.com>
2026-07-13 19:20:44 +0200

    refactor: extraction de la section Accounts en composant

--- frontend/src/pages/Admin/User/Profile.jsx
+++ frontend/src/pages/Admin/User/Profile.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from '@/store/authSlice'
 import { useState } from 'react'
+import Accounts from '@/components/Accounts'
 
 const Profile = () => {
     const firstName = useSelector((state) => state.auth.firstName)
@@ -76,37 +77,7 @@ const Profile = () => {
                     </>
                 )}
             </div>
-            <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                    <p className="account-amount">$2,082.79</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                    <p className="account-amount">$10,928.42</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                    <p className="account-amount">$184.30</p>
-                    <p className="account-amount-description">Current Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
+            <Accounts />
         </main>
     );
 };
